635b9198d421e550ae943cb951daaa295d8f2034
Joachim Køcher Kelsen <joachim@kelsen.nu>
2021-10-08 16:32:13 +0200

    add: global css

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,4 +1,5 @@
 module.exports = {
+	from: 'global.css',
 	plugins: {
 		autoprefixer: {},
 		tailwindcss: {},
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,28 @@
 module.exports = {
+	theme: {
+		extend: {
+			colors: {
+				"bug": "var(--bug)",
+				"dark": "var(--dark)",
+				"dragon": "var(--dragon)",
+				"electric": "var(--electric)",
+				"fairy": "var(--fairy)",
+				"fighting": "var(--fighting)",
+				"fire": "var(--fire)",
+				"flying": "var(--flying)",
+				"ghost": "var(--ghost)",
+				"grass": "var(--grass)",
+				"ground": "var(--ground)",
+				"ice": "var(--ice)",
+				"normal": "var(--normal)",
+				"poison": "var(--poison)",
+				"psychic": "var(--psychic)",
+				"rock": "var(--rock)",
+				"steel": "var(--steel)",
+				"water": "var(--water)",
+			},
+		},
+	},
 	mode: 'jit',
 	purge: ['./src/**/*.svelte'],
 }
