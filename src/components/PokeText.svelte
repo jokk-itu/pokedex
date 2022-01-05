2196cafa20abe0bd7a94956aa4340585319bf089
Joachim Køcher Kelsen <joachim@kelsen.nu>
2022-01-05 21:27:33 +0100

    add: support as a progressive web app

--- src/components/PokeText.svelte
+++ src/components/PokeText.svelte
@@ -9,7 +9,7 @@
     }
 </style>
 
-<h1 class='title text-yellow-400 text-8xl text-center my-8'>
+<h1 class='w-full title text-yellow-400 lg:text-8xl text-5xl text-center my-8'>
 {#each text as t, i}
 	<span class={(i & 1) === 0 ? 'uppercase' : 'lowercase'}>{t}</span>
 {/each}
